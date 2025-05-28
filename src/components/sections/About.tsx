d1e43c785b61a09241afb368fada007d4eaa3453
Giresh Raju <141443281+gireshraju@users.noreply.github.com>
2025-05-29 00:44:16 +0530

    Update About.tsx

--- src/components/sections/About.tsx
+++ src/components/sections/About.tsx
@@ -22,7 +22,7 @@ const About: React.FC = () => {
           >
             <div className="w-64 h-64 md:w-80 md:h-80 mx-auto rounded-full overflow-hidden border-4 border-gold">
               <img 
-                src="gireshraju.jpg" 
+                src="GireshRaju.jpg" 
                 alt="Giresh Raju Adimulam" 
                 className="w-full h-full object-cover"
               />
